86a7018802a4b5e0c95d03a75e6e6475470d1c61
Sergei Pchelintsev <s.pchelintcev@yandex.ru>
2022-07-31 14:56:48 +0300

    обрезанное среднее

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,13 @@
 public class Main {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
-        SalesManager week01 = new SalesManager(new int[] {15, 20, 21, 14, 16, 25, 30});
+        SalesManager week01 = new SalesManager(new int[]{15, 20, 21, 14, 16, 25, 30});
         System.out.println("максимальные продажи за указанный период: " + week01.max());
+        System.out.println("минимальные продажи за указанный период: " + week01.min());
+        System.out.println("общая сумма продажи за указанный период: " + week01.sum());
+        System.out.println("размер указанного периода: " + week01.size());
+        System.out.println("сумма обрезанного среднего: " + (week01.sum() - week01.max() - week01.min()) / (week01.size() - 2));
+
     }
 }
--- src/SalesManager.java
+++ src/SalesManager.java
@@ -1,15 +1,43 @@
 public class SalesManager {
     protected int[] sales;
-    public SalesManager(int[] sales){
+
+    public SalesManager(int[] sales) {
         this.sales = sales;
     }
-    public int max(){
+
+    public int max() {
         int max = -1;
-        for(int sale : sales){
-            if(sale > max){
+        for (int sale : sales) {
+            if (sale > max) {
                 max = sale;
             }
         }
         return max;
+    }
+
+    //TODO минимальное значение:
+    public int min() {
+        int min = sales[0] + 1;
+        for (int sale : sales) {
+            if (sale < min) {
+                min = sale;
+            }
+        }
+        return min;
+    }
+
+    //TODO сумма всех ячеек:
+    public int sum() {
+        int sum = 0;
+        for (int sale : sales) {
+            sum += sale;
+        }
+        return sum;
+    }
+
+    //TODO размер массива:
+    public int size() {
+        int size = sales.length;
+        return size;
     }
 }
